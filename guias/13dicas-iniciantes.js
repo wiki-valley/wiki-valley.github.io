e8a0c2b58d8c87886068275456051b2bcd502ad9
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2023-08-01 16:48:36 -0300

    animations

--- guias/13dicas-iniciantes.js
+++ guias/13dicas-iniciantes.js
@@ -1,12 +1,13 @@
-const elementoFixo = document.querySelector('.recommended');
-const distanciaTopo = 100; // Distância do topo da página onde o elemento deve começar a se mover
-
 window.addEventListener('scroll', function() {
-    if (window.scrollY > distanciaTopo) {
-        elementoFixo.style.top = '20px'; 
-        elementoFixo.style.position = fixed;
-        // Distância do elemento para o topo da tela quando começa a se mover
-    } else {
-        elementoFixo.style.top = distanciaTopo - window.scrollY + 'px';
-    }
+    const guides = document.querySelectorAll('.guide');
+
+    const distanciaTopo = 100; // Distância do topo da página onde o elemento deve começar a se mover
+
+    guides.forEach(guide => {
+        if (window.scrollY > distanciaTopo) {
+            guide.classList.add('guide-fixo');
+        } else {
+            guide.classList.remove('guide-fixo');
+        }
+    });
 });
