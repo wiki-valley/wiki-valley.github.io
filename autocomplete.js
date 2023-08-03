fc6e0744efa12a6dcdc8bd45a118e0975a08c679
Cauadsm <cauamoraesdev2023@gmail.com>
2023-08-03 15:23:31 -0300

    Repair href
    Coloquei o links para redirecionar para o caminho do site no github.

--- autocomplete.js
+++ autocomplete.js
@@ -1,76 +1,76 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const palavraChaves = [
-        { text: 'Guia Introdução', link: 'http://localhost:5500/guias/tables/guia-introducao.html' },
-        { text: 'Jogador', link: 'http://localhost:5500/guias/tables/oJogador.html' },
-        { text: 'Energia', link: '' },
-        { text: 'Ciclo do Dia', link: '' },
-        { text: 'Saúde', link: '' },
-        { text: 'Inventário', link: '' },
-        { text: 'Controles', link: '' },
-        { text: 'Habilidades', link: '' },
-        { text: 'Mapas da Fazenda', link: '' },
-        { text: 'Lavouras', link: '' },
-        { text: 'Arvores Frutiferas', link: '' },
-        { text: 'Caverna', link: '' },
-        { text: 'Entregas', link: '' },
-        { text: 'Mercadorias Artesanais', link: '' },
-        { text: 'Estufa', link: '' },
-        { text: 'Animais', link: '' },
-        { text: 'Casa da Fazenda', link: '' },
-        { text: 'Casa', link: '' },
-      ];
-      
-      const resultsBox = document.querySelector(".result-box");
-      const inputBox = document.getElementById("input-box");
+  const palavraChaves = [
+    { text: 'Guia Introdução', link: 'https://wiki-valley.github.io/guias/tables/guia-introducao.html' },
+    { text: 'Jogador', link: 'https://wiki-valley.github.io/guias/tables/oJogador.html' },
+    { text: 'Energia', link: '' },
+    { text: 'Ciclo do Dia', link: '' },
+    { text: 'Saúde', link: '' },
+    { text: 'Inventário', link: '' },
+    { text: 'Controles', link: '' },
+    { text: 'Habilidades', link: '' },
+    { text: 'Mapas da Fazenda', link: '' },
+    { text: 'Lavouras', link: '' },
+    { text: 'Arvores Frutiferas', link: '' },
+    { text: 'Caverna', link: '' },
+    { text: 'Entregas', link: '' },
+    { text: 'Mercadorias Artesanais', link: '' },
+    { text: 'Estufa', link: '' },
+    { text: 'Animais', link: '' },
+    { text: 'Casa da Fazenda', link: '' },
+    { text: 'Casa', link: '' },
+  ];
 
-        // Funçoes para ativar e desativar o Display
-      function showDisplay() {
-        resultsBox.style.display = "block";
-      }
-      function hideDisplay() {
-        resultsBox.style.display = "none";
-      }
+  const resultsBox = document.querySelector(".result-box");
+  const inputBox = document.getElementById("input-box");
 
-      inputBox.onkeyup = function(event){
-          let result = [];
-          let input = inputBox.value; 
-          if(input.length){
-              result = palavraChaves.filter((chavefodida)=>{
-                  return chavefodida.text.toLowerCase().includes(input.toLowerCase());
-              });
-            }
-          display(result);
-          enterKeyPress(event,result);
-      }
-      
-      function display(result){
-          const content = result.map((chavefodida)=>{
-              return `<li><a href="${chavefodida.link}">${chavefodida.text}</a></li>`;
-          });
+  // Funçoes para ativar e desativar o Display
+  function showDisplay() {
+    resultsBox.style.display = "block";
+  }
+  function hideDisplay() {
+    resultsBox.style.display = "none";
+  }
 
-          //Ocultar resultando quando o valor for vazio.
-        if (content.length === 0){
-            hideDisplay();
-        }else{
-            showDisplay();
+  inputBox.onkeyup = function (event) {
+    let result = [];
+    let input = inputBox.value;
+    if (input.length) {
+      result = palavraChaves.filter((chavefodida) => {
+        return chavefodida.text.toLowerCase().includes(input.toLowerCase());
+      });
+    }
+    display(result);
+    enterKeyPress(event, result);
+  }
 
-            const limitedResults = result.slice(0, 5);
+  function display(result) {
+    const content = result.map((chavefodida) => {
+      return `<li><a href="${chavefodida.link}">${chavefodida.text}</a></li>`;
+    });
 
-            const content = limitedResults.map((chavefodida) =>{
-                return `<p><a href="${chavefodida.link}">${chavefodida.text}</a></p>`
-            })
-            resultsBox.innerHTML = content.join("");
-        }
-    
-      }
-    
-      function enterKeyPress(event, result) {
-        //Verificar se a tecla enter foi pressionada.
-        if(event.keyCode === 13 && result.length === 1){
-            //Se ouver apenas um resultado e a tecla enter for pressionada redirecionar para o link.
-            window.location.href = result[0].link;
-        }
-      }
+    //Ocultar resultando quando o valor for vazio.
+    if (content.length === 0) {
+      hideDisplay();
+    } else {
+      showDisplay();
 
-  });
+      const limitedResults = result.slice(0, 5);
+
+      const content = limitedResults.map((chavefodida) => {
+        return `<p><a href="${chavefodida.link}">${chavefodida.text}</a></p>`
+      })
+      resultsBox.innerHTML = content.join("");
+    }
+
+  }
+
+  function enterKeyPress(event, result) {
+    //Verificar se a tecla enter foi pressionada.
+    if (event.keyCode === 13 && result.length === 1) {
+      //Se ouver apenas um resultado e a tecla enter for pressionada redirecionar para o link.
+      window.location.href = result[0].link;
+    }
+  }
+
+});
